cb3db6572e74682216315bf7c3873c3ae6412e91
lemonilemon <imlemonilemon@gmail.com>
2023-10-03 12:51:22 +0000

    update nix settings

--- general/nix/default.nix
+++ general/nix/default.nix
@@ -1,9 +1,25 @@
-{username, ...}:
 {
+    username,
+    ...
+}: {
     nix = {
-        extraOptions = ''
-            experimental-features = nix-command flakes
-            trusted-users = [ ${username} ];
-        '';
+        settings = {
+            trusted-users = [username];
+              # FIXME: use your access tokens from secrets.json here to be able to clone private repos on GitHub and GitLab
+              # access-tokens = [
+              #   "github.com=${secrets.github_token}"
+              #   "gitlab.com=OAuth2:${secrets.gitlab_token}"
+              # ];
+
+            accept-flake-config = true;
+            auto-optimise-store = true;
+        };
+
+        extraOptions = ''experimental-features = nix-command flakes'';
+
+        gc = {
+            automatic = true;
+            options = "--delete-older-than 7d";
+        };
     };
 }

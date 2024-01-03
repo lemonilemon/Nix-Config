92d7665c47e91fc58f76adc3c9998227ae891bbb
lemonilemon <imlemonilemon@gmail.com>
2024-01-03 12:19:59 +0000

    super-tab mapping for completion

--- home-manager/nvim/source/init.lua
+++ home-manager/nvim/source/init.lua
@@ -9,6 +9,7 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 vim.cmd([[
@@ -127,7 +128,6 @@ local plugins = {
             vim.g.transparent_enabled = true
         end,
     },
-
 
     {
         "neovim/nvim-lspconfig",
@@ -416,6 +416,12 @@ local plugins = {
             "saadparwaiz1/cmp_luasnip",
         },
         opts = function()
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
+            local luasnip = require("luasnip")
             local cmp = require("cmp")
             return {
                 snippet = {
@@ -434,6 +440,31 @@ local plugins = {
                         { name = "path" },
                     }
                 ),
+                mapping = {
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                        cmp.select_next_item()
+                      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
+                      -- that way you will only jump inside the snippet region
+                      elseif luasnip.expand_or_jumpable() then
+                        luasnip.expand_or_jump()
+                      elseif has_words_before() then
+                        cmp.complete()
+                      else
+                        fallback()
+                      end
+                    end, { "i", "s" }),
+
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                      if cmp.visible() then
+                        cmp.select_prev_item()
+                      elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
+                      else
+                        fallback()
+                      end
+                    end, { "i", "s" }),
+                },
             }
         end,
     },

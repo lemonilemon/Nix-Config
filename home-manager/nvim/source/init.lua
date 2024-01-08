913935b1e095e8ae253e419b51d6b7d5bb47b3ef
lemonilemon <imlemonilemon@gmail.com>
2024-01-09 01:02:31 +0800

    update keymap for nvim-cmp

--- home-manager/nvim/source/init.lua
+++ home-manager/nvim/source/init.lua
@@ -542,31 +542,13 @@ local plugins = {
                 experimental = {
                     ghost_text = true,
                 },
-                mapping = {
-                    ["<Tab>"] = cmp.mapping(function(fallback)
-                      if cmp.visible() then
-                        cmp.select_next_item()
-                      -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable() 
-                      -- that way you will only jump inside the snippet region
-                      elseif luasnip.expand_or_jumpable() then
-                        luasnip.expand_or_jump()
-                      elseif has_words_before() then
-                        cmp.complete()
-                      else
-                        fallback()
-                      end
-                    end, { "i", "s" }),
-
-                    ["<S-Tab>"] = cmp.mapping(function(fallback)
-                      if cmp.visible() then
-                        cmp.select_prev_item()
-                      elseif luasnip.jumpable(-1) then
-                        luasnip.jump(-1)
-                      else
-                        fallback()
-                      end
-                    end, { "i", "s" }),
-                },
+                mapping = cmp.mapping.preset.insert({
+                  ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                  ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                  ['<C-Space>'] = cmp.mapping.complete(),
+                  ['<C-e>'] = cmp.mapping.abort(),
+                  ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
             }
         end,
     },

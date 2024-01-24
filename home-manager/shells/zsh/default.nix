6d8c99ad2d04e657de4e284608906543c00b6862
lemonilemon <imlemonilemon@gmail.com>
2024-01-24 13:42:05 +0800

    use function instead of alias

--- home-manager/shells/zsh/default.nix
+++ home-manager/shells/zsh/default.nix
@@ -14,12 +14,6 @@
             ls="ls --color=auto";
             lg="lazygit";
             ".." = "cd ..";
-            update="cd /home/${username}/nixos-config 
-                    git pull
-                    sudo nixos-rebuild switch --flake .#${sys}
-                    nix flake update
-                    source /home/${username}/.zshrc
-                    zplug update";
         };
         enableCompletion = false;
         zplug = {
@@ -51,6 +45,17 @@
  
         '';
         initExtra = ''
+            # Functions for zsh
+            update() {
+                cd /home/${username}/nixos-config 
+                git pull
+                sudo nixos-rebuild switch --flake .#${sys}
+                nix flake update
+                source /home/${username}/.zshrc
+                zplug update
+                nvim --headless "+Lazy! sync" +qa
+            }
+
             export LANGUAGE=en_US.UTF-8
             export LC_ALL=en_US.UTF-8
             export LANG=en_US.UTF-8

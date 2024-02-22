023aa961aa0dd5aa276c2ab32f0f30be40d65a8b
lemonilemon <imlemonilemon@gmail.com>
2024-02-23 00:35:26 +0800

    minor fix

--- home-manager/zellij/default.nix
+++ home-manager/zellij/default.nix
@@ -3,6 +3,7 @@
 }: {
     programs.zellij = {
         enable = true;
+        enableZshIntegration = true;
         settings = {
             theme = "catppuccin-mocha";
             default_shell = "zsh";

e0f8e303d211f978b2be8ec7ffeb8dcf985092b2
lemonilemon <imlemonilemon@gmail.com>
2024-03-04 22:58:27 +0800

    update conform.lua for js formatting

--- home-manager/nvim/source/lua/plugins/code/conform.lua
+++ home-manager/nvim/source/lua/plugins/code/conform.lua
@@ -20,6 +20,9 @@ return {
             lua = { "stylua" },
             python = { "isort", "black" },
             javascript = { { "prettierd", "prettier" } },
+            typescript = { { "prettierd", "prettier" } },
+            javascriptreact = { { "prettierd", "prettier" } },
+            typescriptreact = { { "prettierd", "prettier" } },
         },
         -- Set up format-on-save
         format_on_save = { timeout_ms = 500, lsp_fallback = true },

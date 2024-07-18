8a36f633a23f2b4e751e238af739d5afc41d8a42
lemonilemon <imlemonilemon@gmail.com>
2024-07-18 13:11:31 +0800

    Update: fix clipboard for distro other than WSL

--- home-manager/nvim/source/lua/options/base.lua
+++ home-manager/nvim/source/lua/options/base.lua
@@ -70,17 +70,3 @@ autocmd({
 });
 
 opt.clipboard = "unnamedplus";
-vim.cmd([[
-    let g:clipboard = {
-    \	'name': 'win32yank-wsl',
-    \	'copy': {
-    \		'+': 'win32yank.exe -i --crlf',
-    \		'*': 'win32yank.exe -i --crlf',
-    \	},
-    \	'paste': {
-    \      '+': 'win32yank.exe -o --lf',
-    \      '*': 'win32yank.exe -o --lf',
-    \   },
-    \	'cache_enabled': 1,
-    \}
-]]);

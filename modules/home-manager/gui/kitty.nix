0ea44d2e7e138079e6da60fd317a70f683f0af06
lemonilemon <imlemonilemon@gmail.com>
2025-02-21 09:58:54 +0800

    feat(kitty): add settings and modify font size

--- modules/home-manager/gui/kitty.nix
+++ modules/home-manager/gui/kitty.nix
@@ -10,11 +10,21 @@
       enable = true;
       themeFile = "Catppuccin-Mocha";
       font = {
-        size = 14;
+        size = 16;
         name = "FiraCode Nerd Font";
       };
       keybindings = {
         "ctrl+c" = "copy_or_interrupt";
+      };
+      settings = {
+        confirm_os_window_close = 0;
+        dynamic_background_opacity = true;
+        enable_audio_bell = false;
+        mouse_hide_wait = "-1.0";
+        window_padding_width = 10;
+        background_opacity = "0.7";
+        strip_trailing_spaces = "smart";
+        paste_actions = "confirm-if-large";
       };
       shellIntegration.enableZshIntegration = true;
     };

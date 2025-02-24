3a4f19c0dc493e049e1c10b585f4610c3440a975
lemonilemon <imlemonilemon@gmail.com>
2025-02-24 16:19:54 +0800

    feat(hyprlock): use hyprlock in wlogout

---
+++ modules/home-manager/gui/desktop/hyprland/hyprlock.nix
@@ -0,0 +1,8 @@
+{
+  ...
+}:
+{
+  programs.hyprlock = {
+    enable = true;
+  };
+}
--- modules/home-manager/gui/desktop/hyprland/wlogout/default.nix
+++ modules/home-manager/gui/desktop/hyprland/wlogout/default.nix
@@ -3,6 +3,9 @@
   ...
 }:
 {
+  imports = [
+    ../hyprlock.nix
+  ];
   # Icons
   home.file.".config/wlogout/icons" = {
     enable = true;
@@ -12,5 +15,43 @@
   # Logout menu
   programs.wlogout = {
     enable = true;
+    layout = [
+      {
+        "label" = "lock";
+        "action" = "hyprlock";
+        "text" = "Lock";
+        "keybind" = "l";
+      }
+      {
+        "label" = "hibernate";
+        "action" = "systemctl hibernate";
+        "text" = "Hibernate";
+        "keybind" = "h";
+      }
+      {
+        "label" = "logout";
+        "action" = "loginctl terminate-user $USER";
+        "text" = "Logout";
+        "keybind" = "e";
+      }
+      {
+        "label" = "shutdown";
+        "action" = "systemctl poweroff";
+        "text" = "Shutdown";
+        "keybind" = "s";
+      }
+      {
+        "label" = "suspend";
+        "action" = "systemctl suspend";
+        "text" = "Suspend";
+        "keybind" = "u";
+      }
+      {
+        "label" = "reboot";
+        "action" = "systemctl reboot";
+        "text" = "Reboot";
+        "keybind" = "r";
+      }
+    ];
   };
 }

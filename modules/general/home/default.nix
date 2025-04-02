7424b74f6e7af16213ac0e1c96af124743fc0068
lemonilemon <imlemonilemon@gmail.com>
2025-04-03 02:54:17 +0800

    fix: fix python bug of libc

--- modules/general/home/default.nix
+++ modules/general/home/default.nix
@@ -19,11 +19,12 @@
     # Additional program languages I use
     home.general-settings.programlangs.packages = with pkgs; [
       gcc # c/c++
-      (python3.withPackages (
-        ps: with ps; [
-          numpy
-        ]
-      )) # python
+      python3
+      # (python3.withPackages (
+      #   ps: with ps; [
+      #     # numpy
+      #   ]
+      # )) # python
       uv # python package
       nodejs # nodejs
       jre8 # Java Development Kit

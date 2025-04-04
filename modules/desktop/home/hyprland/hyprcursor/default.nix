7f5b7b27b53862e707da02bb7c21e8ed2b8ebd83
lemonilemon <imlemonilemon@gmail.com>
2025-04-04 18:53:59 +0800

    fix: consistent cursor size

--- modules/desktop/home/hyprland/hyprcursor/default.nix
+++ modules/desktop/home/hyprland/hyprcursor/default.nix
@@ -10,18 +10,17 @@
   home.pointerCursor = {
     enable = true;
     name = "rose-pine-hyprcursor";
-    size = 32;
     package = inputs.rose-pine-hyprcursor.packages.${pkgs.system}.default;
     hyprcursor = {
       enable = true;
-      size = 32;
+      size = 36;
     };
   };
   wayland.windowManager.hyprland = {
     settings = {
       exec-once = [
         "gsettings set org.gnome.desktop.interface cursor-theme BreezeX-RosePine-Linux"
-        "gsettings set org.gnome.desktop.interface cursor-size 32"
+        "gsettings set org.gnome.desktop.interface cursor-size 24"
       ];
     };
   };
@@ -38,7 +37,7 @@
     cursorTheme = {
       name = "rose-pine-cursor";
       package = pkgs.rose-pine-cursor;
-      size = 32;
+      size = 28;
     };
   };
 }

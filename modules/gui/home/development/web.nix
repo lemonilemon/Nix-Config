ed53572d8944128a338a7e9b3705557a304fa77b
lemonilemon <imlemonilemon@gmail.com>
2025-09-17 15:49:49 +0800

    feat: install postgresql and pgadmin

--- modules/gui/home/development/web.nix
+++ modules/gui/home/development/web.nix
@@ -8,6 +8,8 @@
   config = lib.mkIf config.home.gui-settings.development.web.enable {
     home.packages = with pkgs; [
       postman # API development tool
+      postgresql
+      pgadmin4-desktopmode # App to develop postgreSQL
     ];
   };
 }

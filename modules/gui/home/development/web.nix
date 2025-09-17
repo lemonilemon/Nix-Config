0dbd60414b8e691c22b1dad9706da34f3a25f1e4
lemonilemon <imlemonilemon@gmail.com>
2025-09-17 17:17:17 +0800

    feat: install wireshark but removes postgresql (managed with container)

--- modules/gui/home/development/web.nix
+++ modules/gui/home/development/web.nix
@@ -8,8 +8,7 @@
   config = lib.mkIf config.home.gui-settings.development.web.enable {
     home.packages = with pkgs; [
       postman # API development tool
-      postgresql
-      pgadmin4-desktopmode # App to develop postgreSQL
+      wireshark # Network protocol analyzer
     ];
   };
 }

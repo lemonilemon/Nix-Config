30e5c9362dc0ff36417fc9c3b646f54dad1042ec
lemonilemon <imlemonilemon@gmail.com>
2026-01-24 02:13:35 +0800

    chore: update flake.lock and corresponding settings

--- modules/cli/home/nvim/plugins/lsp/lsp.nix
+++ modules/cli/home/nvim/plugins/lsp/lsp.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
 
-  home.packages = with pkgs; [ nixfmt-rfc-style ];
+  home.packages = with pkgs; [ nixfmt ];
   programs.nixvim.plugins = {
     lsp-format = {
       enable = true;
@@ -37,7 +37,7 @@
         nixd = {
           enable = true;
           settings = {
-            formatting.command = [ "nixfmt-rfc-style" ];
+            formatting.command = [ "nixfmt" ];
           };
         };
         marksman = {

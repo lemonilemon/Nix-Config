93c4b409b6ba153e1c993c088fa37736aa7ceaf1
lemonilemon <imlemonilemon@gmail.com>
2026-01-27 12:21:12 +0800

    feat: update atuin settings

--- modules/cli/home/programs/atuin.nix
+++ modules/cli/home/programs/atuin.nix
@@ -2,5 +2,11 @@
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
+    daemon.enable = true;
+    settings = {
+      auto_sync = true;
+      sync_frequency = "5m";
+      sync_address = "https://api.atuin.sh";
+    };
   };
 }

a0fb1503301198ef26957da4bc8e64197e890de4
lemonilemon <imlemonilemon@gmail.com>
2026-01-31 20:11:16 +0800

    fix: atuin on WSL

--- modules/cli/home/programs/atuin.nix
+++ modules/cli/home/programs/atuin.nix
@@ -1,8 +1,12 @@
 {
+  isWSL,
+  ...
+}:
+{
   programs.atuin = {
     enable = true;
     enableZshIntegration = true;
-    daemon.enable = true;
+    daemon.enable = !isWSL;
     settings = {
       auto_sync = true;
       sync_frequency = "5m";

4099492022bf097a0cc08f0a335cb769b18484dd
lemonilemon <imlemonilemon@gmail.com>
2026-02-02 14:27:18 +0800

    fix: treesitter settings for new version

--- modules/cli/home/nvim/plugins/treesitter/treesitter.nix
+++ modules/cli/home/nvim/plugins/treesitter/treesitter.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }:
 {
   # https://nix-community.github.io/nixvim/plugins/treesitter/index.html
+  home.packages = with pkgs; [
+    tree-sitter
+  ];
   programs.nixvim = {
-    filetype.extension.liq = "liquidsoap";
-
     plugins.treesitter = {
       enable = true;
-
       settings = {
         indent = {
           enable = true;
@@ -18,19 +18,8 @@
           ];
         };
       };
-
       folding.enable = false;
-      languageRegister.liq = "liquidsoap";
       nixvimInjections = true;
-      grammarPackages = pkgs.vimPlugins.nvim-treesitter.allGrammars;
     };
-
-    extraConfigLua = ''
-      local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-
-      parser_config.liquidsoap = {
-        filetype = "liquidsoap",
-      }
-    '';
   };
 }

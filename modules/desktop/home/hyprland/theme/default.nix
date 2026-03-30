1009a95703ea865281f231fdb7240c07057f9492
lemonilemon <imlemonilemon@gmail.com>
2026-03-30 21:03:09 +0800

    chore: fix deprecated config

--- modules/desktop/home/hyprland/theme/default.nix
+++ modules/desktop/home/hyprland/theme/default.nix
@@ -42,8 +42,11 @@
         gtk-application-prefer-dark-theme = 1;
       };
 
-      gtk4.extraConfig = {
-        gtk-application-prefer-dark-theme = 1;
+      gtk4 = {
+        theme = config.gtk.theme;
+        extraConfig = {
+          gtk-application-prefer-dark-theme = 1;
+        };
       };
     };
 
